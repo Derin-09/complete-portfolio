4049cb1c0941e1f01d885358440662e867291beb
Derin <aderinsolasamashimi@gmail.com>
2025-08-19 15:58:04 +0100

    designed the sections layout

--- components/ui/layout.tsx
+++ components/ui/layout.tsx
@@ -1,9 +1,18 @@
 import React, { ReactNode } from 'react'
 
-export default function BackgroundLayout ({children}: {children: ReactNode}) {
+export default function BackgroundLayout({ children }: { children: ReactNode }) {
   return (
-    <div className='rounded-2xl bg-[#141414] '>
-        {children}
+    <div
+      className="
+        rounded-2xl 
+        bg-[#141414] 
+        hover:shadow-[0_0_20px_rgba(168,85,247,0.3)]
+        hover:scale-[90%]
+        active:shadow-[0_0_20px_rgba(168,85,247,0.3)]
+        transition-shadow duration-300
+      "
+    >
+      {children}
     </div>
   )
 }
