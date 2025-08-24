f692934cbd24af33e68d85f21073726123dbe46c
Derin <aderinsolasamashimi@gmail.com>
2025-08-24 23:58:39 +0100

    added experience

---
+++ components/motions/InfiniteVertical.tsx
@@ -0,0 +1,44 @@
+"use client"
+import React, { useEffect, useRef, useState } from "react"
+import { motion } from "framer-motion"
+
+type InfiniteType = {
+  children: React.ReactNode
+  speed?: number
+  gap?: number
+}
+
+export default function InfiniteVl({ children, speed = 30, gap = 20 }: InfiniteType) {
+  const items = React.Children.toArray(children)
+  const containerRef = useRef<HTMLDivElement>(null)
+  const [width, setWidth] = useState(0)
+
+    useEffect(() => {
+    if (containerRef.current) {
+      setWidth(containerRef.current.scrollWidth / 2) 
+    }
+  }, [children])
+
+
+  if (!items.length) return null
+
+  return (
+    <div className="overflow-hidden w-full">
+      <motion.div
+        ref={containerRef}
+        className="flex"
+        animate={{ x: [0, -width] }}
+        transition={{
+          repeat: Infinity,
+          ease: "linear",
+          duration: speed,
+        }}
+        style={{ gap }}
+      >
+        {[...items, ...items].map((child, idx) => (
+          <div key={idx}>{child}</div>
+        ))}
+      </motion.div>
+    </div>
+  )
+}
--- components/sections/Experience.tsx
+++ components/sections/Experience.tsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import BackgroundLayout from '../ui/layout'
+import Image from 'next/image'
+import Inspo from '@/public/images/inspo.png'
+import InfiniteVertical from '../motions/InfiniteVertical'
+
+  
+  const content = [
+      {
+        text: "Inventors Community",
+        period: "01/25 - present"
+      },
+      {
+        text: "NextGen Hackathon",
+        period: "06/25 - present"
+      },
+      {
+        text: "Accessivo",
+        period: "08/25 - present"
+      },
+    ]
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <main className='mt-5 w-full select-none'>
+      <BackgroundLayout>
+        <section className='flex flex-col gap-4 items-center justify-center p-5'>
+          <div className='flex flex-col items-center'>
+          <div className="inline-flex items-center gap-2 px-3 py-1 rounded-full mb-2">
+            <Image
+              src={Inspo}
+              width={16}
+              height={16}
+              alt=""
+              className="object-contain"
+            />
+            <p className="text-sm leading-none">Experience</p>
+          </div>
+          <p className='mb-7'>Solutions Suites</p>
+          </div>
+          
+          <InfiniteVertical speed={5} gap={15}>
+            { content.map((c, idx) => (
+            <div key={idx} className="w-[200px] h-[70px] rounded-xl p-2 text-[#916CE7] bg-[#191919]">
+              <p className='font-semibold'> {c.text}</p>  <p className=' text-[#999999]'> {c.period} </p>
+            </div>
+          ))}
+          </InfiniteVertical>
+        </section>
+      </BackgroundLayout>
+    </main>
   )
 }
 
